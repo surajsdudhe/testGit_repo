025164bede8d35410482e092ac4dbc946a33b716
netware <netware@DESKTOP-E9M0GDP>
2022-10-13 00:20:27 +0530

    test

--- gitTest/src/com/gitTest/Main.java
+++ gitTest/src/com/gitTest/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello git.. ab to maan ja");  
-		System.out.println("hello git.. ek bar liya ab firse try karate"); 
+		System.out.println("hello git.. ek bar liya ab f irse try karate"); 
 
 	}
 
